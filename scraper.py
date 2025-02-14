3b115828b400c3bc36c57865a990f8d80a52db40
JBL168 <154274810+JBL168@users.noreply.github.com>
2025-02-14 11:49:47 -0600

    Cleaned up the code a bit; added a few comments

--- scraper.py
+++ scraper.py
@@ -6,35 +6,42 @@ from urllib.parse import urljoin
 
 BASE_URL = 'https://mymarketnews.ams.usda.gov/'
 
+# User Agent because MMN AMS blocks GET requests that look like bots
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0'
 }
 
+# Loop through each page in the database
 for page_num in range(0,8):
-    URL = f'https://mymarketnews.ams.usda.gov/filerepo/reports?name=SJ_LS850&page={page_num}'
+    url = f'https://mymarketnews.ams.usda.gov/filerepo/reports?name=SJ_LS850&page={page_num}'
     download_folder = 'National_Feeder_Stocker_Cattle_Summary'
     os.makedirs(download_folder, exist_ok=True)
 
     try:
-        db_page = requests.get(URL, headers=headers)
+        # GET request for the database page
+        db_page = requests.get(url, headers=headers)
         db_page.raise_for_status()
 
         soup = BeautifulSoup(db_page.content,'html.parser')
+
+        # Find each element that contains the hyperlink
         td_elements = soup.find_all('td', class_='views-field views-field-field-document')
 
         for td in td_elements:
             hyperlink = td.find('a', href=True)
-            RELATIVE_URL = hyperlink['href']
+            relative_url = hyperlink['href']
 
             if hyperlink:
-                full_href = urljoin(BASE_URL, RELATIVE_URL)
+                full_href = urljoin(BASE_URL, relative_url)
                 try:
+                    # GET request for the .txt page
                     txt_page =  requests.get(full_href, headers=headers)
                     txt_page.raise_for_status()
 
-                    file_name = os.path.basename(RELATIVE_URL)
+                    file_name = os.path.basename(relative_url)
                     file_path = os.path.join(download_folder, file_name)
 
+                    # Download the .txt from the page
                     with open(file_path, 'w', encoding='utf-8') as file:
                         file.write(txt_page.text)
                     print(f'Saved {file_name}')
