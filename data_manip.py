42b0a7439c737b2bcfbadf2e97a0ee0090f896ac
JBL168 <154274810+JBL168@users.noreply.github.com>
2025-02-27 17:14:50 -0600

    data aggregation mostly complete; need to iron out some bugs regarding data collection.

--- data_manip.py
+++ data_manip.py
@@ -4,7 +4,7 @@ import re
 
 # function to parse the txt file for relevant data
 def parse_usda_file(filepath):
-    with open(filepath) as file:
+    with open(filepath, 'r', encoding = 'utf-8') as file:
         contents = file.read()
 
         # extract date
@@ -12,21 +12,30 @@ def parse_usda_file(filepath):
         week_ending_date = week_match.group(1) if week_match else None
 
         # extract sale data   r'This Week:\s+([\d,]+)\s+([\d,]+)\s+([\d,]+)\s+([\d,]+)'
-        receipts_match = re.search(r'This Week:?\s+([\d,]+)\s+([\d,]+)\s+([\d,]+)\s+([\d,]+)', contents, re.IGNORECASE)
+        receipts_match = re.search(r'This Week:?\s+(?:\*+\s*)?([\d,\.]+)(?:\s*\*+)?\s+(?:\*+\s*)?([\d,\.]+)(?:\s*\*+)?\s+(?:\*+\s*)?([\d,\.]+)(?:\s*\*+)?\s+(?:\*+\s*)?([\d,\.]+)(?:\s*\*+)?', contents, re.IGNORECASE)
+        # print(receipts_match)
         if receipts_match:
-            national_auctions = int(receipts_match.group(1).replace(",", ""))
-            national_total = int(receipts_match.group(4).replace(",", ""))
+            try:
+                national_auctions = int(receipts_match.group(1).replace(',', '').replace('*', ''))
+                national_total = int(receipts_match.group(4).replace(',', '').replace('*', ''))
+            except:
+                print(filepath)
+                exit()
         else:
             national_auctions = None
             national_total = None
 
         # extract block of text after texas
-        texas_match = re.search(r'(TEXAS\s+\d+\.?.*?)(?=\n\s*[A-Z]{2,}|$)', contents, re.DOTALL | re.IGNORECASE)
+        texas_match = re.search(r'(^[ \t]*TEXAS\b.*?)(?=^[ \t]*[^\w]*OKLAHOMA\b)', contents, re.DOTALL | re.IGNORECASE | re.MULTILINE)
         texas_block = texas_match.group(1) if texas_match else ""
+        # print(texas_match.group(1))
 
         # normalize whitespace to account for newlines breaking up the reg exps
-        texas_block_flat = re.sub(r"\s+", " ", texas_block)
+        texas_block_flat = re.sub(r'\s+', ' ', texas_block)
         # print(texas_block_flat)
+
+        # extract texas auctions
+        
 
         # extract steers subsection
         steers_match = re.search(r'Steers:\s+(.*?)(?=Heifers:|$)', texas_block_flat, re.DOTALL | re.IGNORECASE)
@@ -39,7 +48,7 @@ def parse_usda_file(filepath):
 
         # within steers text, parse "medium and large 1" and "medium large 1-2"
         ml1_pattern = r'Medium\s*and\s*Large\s*1\s+(.*?)(?=Medium\s*and\s*Large\s*1-\s*2|$)'
-        ml1_2_pattern = r'Medium\s*and\s*Large\s*1-\s*2\s+(.*)$'
+        ml1_2_pattern = r'Medium\s*and\s*Large\s*1[-–—]\s*2\s+(.*)$'
 
         # steers medium and large 1
         steers_ml1 = re.search(ml1_pattern, steers_text, re.IGNORECASE)
@@ -62,10 +71,10 @@ def parse_usda_file(filepath):
         # print(heifers_ml1_2_text + '\n\n')
 
         # calculate average prices
-        steers_ml1_avg = compute_weighted_avg(steers_ml1_text)
-        steers_ml1_2_avg = compute_weighted_avg(steers_ml1_2_text)
-        heifers_ml1_avg = compute_weighted_avg(heifers_ml1_text)
-        heifers_ml1_2_avg = compute_weighted_avg(heifers_ml1_2_text)
+        steers_ml1_avg = compute_avg(steers_ml1_text)
+        steers_ml1_2_avg = compute_avg(steers_ml1_2_text)
+        heifers_ml1_avg = compute_avg(heifers_ml1_text)
+        heifers_ml1_2_avg = compute_avg(heifers_ml1_2_text)
 
         # print(steers_ml1_avg)
         # print(steers_ml1_2_avg)
@@ -83,16 +92,13 @@ def parse_usda_file(filepath):
         }
 
 # helper function to calculate the weighted average     
-def compute_weighted_avg(text_block):
-    sale_matches = re.findall(r'\(\s*(\d+)\s*\)\s*([\d]+\.[\d]+)', text_block)
+def compute_avg(text_block):
+    sale_matches = re.findall(r'\)\s*([\d]+\.[\d]+)', text_block)
+    # print(text_block + '\n')
     if not sale_matches:
         return None
-    total_head = 0
-    total_dollars = 0
-    for head_str, price_str in sale_matches:
-        total_head += int(head_str)
-        total_dollars += int(head_str) * float(price_str)
-    return round(total_dollars / total_head, 2)
+    sale_matches = [float(price_str) for price_str in sale_matches]
+    return round(sum(sale_matches)/len(sale_matches), 2)
 
 data_rows = []
 data_folder = r'National_Feeder_Stocker_Cattle_Summary'
@@ -102,10 +108,12 @@ for file in os.listdir(data_folder):
     parsed = parse_usda_file(os.path.join(data_folder,file))
     data_rows.append(parsed)
 
-# print(data_rows)
+df = pd.DataFrame(data_rows)
+print(df.to_string())
+# print(df.head())
+
 
 # data = parse_usda_file(r'National_Feeder_Stocker_Cattle_Summary\ams_3232_00001.txt')
 # print(data)
 
-df = pd.DataFrame(data_rows)
-print(df.to_string())
+df.to_csv('aggregated_data.csv', index=False)
