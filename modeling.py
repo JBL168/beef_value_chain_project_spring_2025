13266269dbacdd08f5dbf914a46c282f19d45aab
JBL168 <154274810+JBL168@users.noreply.github.com>
2025-03-05 23:58:12 -0600

    Perform inflation adjustment / data preprocessing

--- modeling.py
+++ modeling.py
@@ -1,4 +1,24 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import optuna 
+import numpy as np
+import optuna as ot
+
+data = pd.read_csv('aggregated_data.csv')
+
+# INFLATION ADJUSTMENT
+base_cpi = data['CPI_US'].iloc[-1]
+data['AVG_PRICE_AUCTION'] = data['AVG_PRICE_AUCTION'] * (base_cpi/data['CPI_US'])
+data['CORN_FUTURES_TYPICAL_PRICE'] = data['CORN_FUTURES_TYPICAL_PRICE'] * (base_cpi/data['CPI_US'])
+data['FEEDER_CATTLE_FUTURES_TYPICAL_PRICE'] = data['FEEDER_CATTLE_FUTURES_TYPICAL_PRICE'] * (base_cpi/data['CPI_US'])
+data['US_MXN_RATES'] = data['US_MXN_RATES'] * (base_cpi/data['CPI_US'])
+data['DIESEL_RETAIL_PRICE'] = data['DIESEL_RETAIL_PRICE'] * (base_cpi/data['CPI_US'])
+data = data.drop('CPI_US', axis=1)
+data['DATE'] = pd.to_datetime(data['DATE'])
+data = data.sort_values('DATE', ignore_index=True).set_index('DATE')
+
+print(data.head())
+
